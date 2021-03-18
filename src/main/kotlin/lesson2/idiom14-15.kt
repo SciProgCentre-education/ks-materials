c5cba1d7163157ce8da4ea012e0e7443d000a30a
Alexander Nozik <altavir@gmail.com>
2021-03-18 16:00:05 +0300

    lesson2

--- src/main/kotlin/lesson2/idiom14-15.kt
+++ src/main/kotlin/lesson2/idiom14-15.kt
@@ -1,5 +1,8 @@
 package lesson2
 
+/**
+ * Extension property on a String. It is not monkey-patching.
+ */
 fun String.countOs(): Int = count { it == 'о' } // implicit this points to String
 
 fun Int.printMe() = println(this) // explicit this
@@ -7,3 +10,17 @@ fun Int.printMe() = println(this) // explicit this
 fun main() {
     "обороноспособность".countOs().printMe()
 }
+
+/**
+ * Extension property (must be virtual)
+ */
+val List<Int>.odd get() = filter { it % 2 == 1 }
+
+/**
+ * Extension variable (also must be virtual)
+ */
+var Array<Int>.second: Int
+    get() = get(1)
+    set(value) {
+        set(1, value)
+    }
